254e0953f3593b6a01839d1f6c148965c9ab827a
leozizz <leonardocps2015@gmail.com>
2022-07-26 22:01:20 -0300

    ajustes nas condicionais

--- Exercicio-2/script.js
+++ Exercicio-2/script.js
@@ -2,8 +2,8 @@ let boletoPago = false
 
 boletoPago = confirm('O boleto foi pago?')
 
-if(boletoPago === false) {
-    alert('O boleto não está pago!')
-} else {
+if(boletoPago) {
     alert('O boleto está pago!')
+} else if (boletoPago === false) {
+    alert('O boleto não está pago!')
 }
